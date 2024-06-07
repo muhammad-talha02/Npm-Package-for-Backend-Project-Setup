939e5a2dbb1db1d7d072dc963bde8d959f33f36e
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-06-08 02:52:13 +0400

    file system done

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,10 @@ import fs from "fs-extra";
 import path from "path";
 import inquirer from "inquirer";
 import { fileURLToPath } from "url";
+import { promisify } from "util";
+import { execSync } from "node:child_process";
+
+// const execAsync = promisify(exec);
 import CommandExecution from "./CommandExecution.js";
 
 //? Es Module -> in ES6 we get Directory Name in this way
@@ -10,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url); // Return File Name
 const __dirname = path.dirname(__filename); // Return Directory Name
 
 //? Get Template Directory path of our template files
-const TEMPLATE_DIR = path.join(__dirname, "..", "template"); // path of our template
+const CURRENT_TEMPLATE_DIR = path.join(__dirname, "..", "template"); // path of our template
 
 const PromoptQuestions = [
   {
@@ -19,31 +23,32 @@ const PromoptQuestions = [
     message: "What is your project name?",
     default: "backend-app",
   },
-  {
-    type: 'password',
-    name: 'password',
-    message: 'Enter a secure password:',
-  },
-  {
-    type: 'list',
-    name: 'theme',
-    message: 'Choose a theme:',
-    choices: ['Light', 'Dark', 'Solarized'],
-  },
 ];
 
 async function main() {
   const answers = await inquirer.prompt(PromoptQuestions);
   console.log("answers", answers);
-  console.log(chalk.blue('Installing dependencies...'));
+  const { projectName } = answers;
 
-//  await CommandExecution("npm install express dotenv")
+  //? Current Target Directory to Setup Project
+  const USER_TARGET_DIR = path.join(process.cwd(), projectName);
 
-//? Current Target Directory to Setup Project
-const targetDir = path.join(process.cwd(), answers.projectName);
+  //? Copy All Template files in new Created ProjectName Folder
+  await fs.copy(CURRENT_TEMPLATE_DIR, USER_TARGET_DIR);
 
-// await fs.copy(TEMPLATE_DIR, targetDir);
+  //? Move into New Created Project Directory to Install Packages
+  process.chdir(USER_TARGET_DIR);
 
+  //? Now Execute Commands (Install Dependencies) in New Created Project
+  console.log(chalk.blue("Installing dependencies..."));
+  //  await CommandExecution("npm install express dotenv")
+  execSync("npm install express dotenv");
+  console.log(
+    chalk.green(`\nSuccess! Created ${projectName} in \n ${USER_TARGET_DIR}\n`)
+  );
+  console.log(`cd ${answers.projectName}`);
+  console.log(chalk.cyan("\n  npm start\n"));
+  console.log(chalk.red("Happy coding!\n"));
 }
 
 main();
